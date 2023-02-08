1c3b6d09a411e2037f3357619beec84414c2cb7d
evnchn <evanchan040511@gmail.com>
2023-02-08 15:46:24 +0800

    Initial commit

---
+++ linkstart.py
@@ -0,0 +1,35 @@
+import_string = \
+'''
+import requests
+from bs4 import BeautifulSoup # beautifulsoup4
+import discord # discord.py
+'''
+### LINK START! (https://github.com/evnchn/linkstart.py)
+for line in import_string.splitlines():
+    if "import" in line:
+        print(line)
+        try:
+            exec(line)
+        except:
+            if "#" in line:
+                package_name = line.split("#")[-1]
+            else:
+                splits = line.split("import")
+                if "from" in line:
+                    package_name = splits[0].replace("from","")
+                else:
+                    package_name = splits[1]
+            package_name = package_name.strip()
+            print("Installing {}...".format(package_name))    
+            import subprocess
+            import sys
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+            try:
+                exec(line)
+            except:
+                print("Failed to install {}".format(package_name))
+### DONE
+
+# The libraries are now guaranteed to be imported
+print(discord)
+print(BeautifulSoup)
